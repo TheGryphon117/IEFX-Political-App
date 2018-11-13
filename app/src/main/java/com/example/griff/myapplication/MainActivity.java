8fcc9fbdfaf26b73b299d198b59bb1bae740d816
TheGryphon117 <griffinsipes@gmail.com>
2018-11-13 16:07:58 -0600

    Poltical App

---
+++ app/src/main/java/com/example/griff/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.griff.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+private WebView mWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.ap.org/en-us/topics/politics/");
+
+    }
+}
